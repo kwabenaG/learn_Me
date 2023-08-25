98fe9178521f05c1d35416d51913613a6b50895d
george <developers.tishbe@gmail.com>
2023-08-25 00:26:56 -0400

    added new directories and add welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:learn_me/pages/welcome/welcome.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 205, 193, 226)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const WelcomePage(),
     );
   }
 }
--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,1 +1,28 @@
 // welcome page
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class WelcomePage extends ConsumerWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(children: [
+        PageView(scrollDirection: Axis.horizontal, children: [
+          Column(children: [
+            // add image here
+            const Text("one"),
+            Container(
+                margin: const EdgeInsets.only(top: 15),
+                child: const Text("AA")),
+            Container(child: [])
+          ]),
+        ]),
+        const Positioned(child: Text("Widget One"))
+      ]),
+    );
+  }
+}
