a8151087bd955aadc272d5f710d7db317627f91f
george <developers.tishbe@gmail.com>
2023-09-03 22:34:09 -0400

    added app colors utils

---
+++ lib/commons/utils/app_colors.dart
@@ -0,0 +1,26 @@
+// app colors
+
+import 'dart:ui';
+
+class AppColor {
+  // add comment here
+  static const Color primaryColor = Color.fromARGB(5, 20, 126, 175);
+
+  // add comment here
+  static const Color primarySecondaryColor = Color.fromARGB(5, 20, 126, 175);
+
+  // main widget color blue
+  static const Color primaryElement = Color.fromARGB(255, 61, 61, 216);
+
+  // main text  color black
+  static const Color primaryText = Color.fromARGB(255, 0, 0, 0);
+
+  // viedo background color
+  static const Color primaryBg = Color.fromARGB(255, 32, 47, 62);
+
+  // main widget text color white
+  static const Color primaryElementText = Color.fromARGB(255, 255, 255, 255);
+
+  static const Color primarySecondaryElementText =
+      Color.fromARGB(255, 102, 102, 102);
+}
